a7361351f2a79e0a4bc5a0ab8ac25e34231774b0
Sarah D. <sarah@example.com>
2025-05-12 12:39:57 +0300

    AuthCubit now uses UseCase instead of AuthRepository

--- lib/features/auth/presentation/bloc/auth_cubit.dart
+++ lib/features/auth/presentation/bloc/auth_cubit.dart
@@ -1,4 +1,9 @@
 import 'package:bloc/bloc.dart';
+import 'package:contact_app1/features/auth/domain/usecases/check_token.dart';
+import 'package:contact_app1/features/auth/domain/usecases/get_token.dart';
+import 'package:contact_app1/features/auth/domain/usecases/login_user.dart';
+import 'package:contact_app1/features/auth/domain/usecases/logout_user.dart';
+import 'package:contact_app1/features/auth/domain/usecases/register_user.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:equatable/equatable.dart';
 import 'package:logger/logger.dart';
@@ -6,47 +11,55 @@ import 'package:meta/meta.dart';
 
 import 'package:contact_app1/features/auth/domain/entities/login_credentials.dart';
 import 'package:contact_app1/features/auth/domain/entities/register_info.dart';
-import 'package:contact_app1/features/auth/domain/repositories/auth_repository.dart';
 
 part 'auth_state.dart';
 
 final logger = Logger();
 
 class AuthCubit extends Cubit<AuthState> {
-  final AuthRepository authRepository;
+  final CheckToken checkTokenUseCase;
+  final GetToken getTokenUseCase;
+  final LoginUser loginUserUseCase;
+  final LogoutUser logoutUserUseCase;
+  final RegisterUser registerUserUseCase;
 
-  AuthCubit({required this.authRepository}) : super(AuthInitial());
+ AuthCubit({
+  required this.getTokenUseCase,
+  required this.loginUserUseCase,
+  required this.logoutUserUseCase,
+  required this.registerUserUseCase,
+  required this.checkTokenUseCase,
+}) : super(AuthInitial());
+
 
   Future<void> login(String email, String password) async {
     emit(AuthLoading());
     final credentials = LoginCredentials(email: email, password: password);
-    final result = await authRepository.login(credentials);
+    final result = await loginUserUseCase(credentials);
 
     result.fold(
       (failure) => emit(AuthFailure(failure.message.tr())),
       (authResponse) async {
         logger.i("Token received successfully: ${authResponse.token}");
         emit(AuthAuthenticated(authResponse.token));
-        await checkAuthStatus();
+        await checkAuthStatus(); // İsteğe bağlı
       },
     );
   }
 
   Future<void> register(RegisterInfo registerInfo) async {
     emit(AuthLoading());
-    final result = await authRepository.register(registerInfo);
+    final result = await registerUserUseCase(registerInfo);
 
     result.fold(
       (failure) => emit(AuthFailure(failure.message.tr())),
-      (authResponse) async {
-        emit(AuthAuthenticated(authResponse.token));
-      },
+      (authResponse) => emit(AuthAuthenticated(authResponse.token)),
     );
   }
 
   Future<void> logout() async {
     emit(AuthLoading());
-    final result = await authRepository.logout();
+    final result = await logoutUserUseCase();
 
     result.fold(
       (failure) => emit(AuthFailure(failure.message.tr())),
@@ -56,7 +69,7 @@ class AuthCubit extends Cubit<AuthState> {
 
   Future<void> checkAuthStatus() async {
     emit(AuthLoading());
-    final result = await authRepository.getToken();
+    final result = await getTokenUseCase();
 
     result.fold(
       (failure) => emit(AuthFailure(failure.message.tr())),
