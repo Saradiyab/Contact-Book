61e730f406b8f9313626f29560565e17826f4bc1
Sarah D. <sarah@example.com>
2025-05-12 12:52:48 +0300

    -Register UseCases and inject them into corresponding Cubits - Added lazy singletons for all domain UseCases (auth, user, contact, company)

--- lib/core/dependency_injection/injection_container.dart
+++ lib/core/dependency_injection/injection_container.dart
@@ -3,6 +3,11 @@ import 'package:contact_app1/features/auth/data/datasources/auth_remote_data_sou
 import 'package:contact_app1/features/auth/data/datasources/auth_service.dart';
 import 'package:contact_app1/features/auth/data/repositories/auth_repository_impl.dart';
 import 'package:contact_app1/features/auth/domain/repositories/auth_repository.dart';
+import 'package:contact_app1/features/auth/domain/usecases/check_token.dart';
+import 'package:contact_app1/features/auth/domain/usecases/get_token.dart';
+import 'package:contact_app1/features/auth/domain/usecases/login_user.dart';
+import 'package:contact_app1/features/auth/domain/usecases/logout_user.dart';
+import 'package:contact_app1/features/auth/domain/usecases/register_user.dart';
 import 'package:contact_app1/features/auth/presentation/bloc/auth_cubit.dart';
 import 'package:contact_app1/features/company/data/datasources/company_remote_data_source.dart';
 import 'package:contact_app1/features/company/data/datasources/company_remote_data_source_impl.dart';
@@ -10,6 +15,8 @@ import 'package:contact_app1/features/company/data/datasources/company_remote_da
 import 'package:contact_app1/features/company/data/datasources/company_service.dart';
 import 'package:contact_app1/features/company/data/repositories/company_repository_impl.dart';
 import 'package:contact_app1/features/company/domain/repositories/company_repository.dart';
+import 'package:contact_app1/features/company/domain/useCases/get_company_details.dart';
+import 'package:contact_app1/features/company/domain/useCases/update_company.dart';
 import 'package:contact_app1/features/company/presentation/bloc/company_cubit.dart';
 import 'package:contact_app1/features/contact/data/datasources/contact_remote_data_source.dart';
 import 'package:contact_app1/features/contact/data/datasources/contact_remote_data_source_impl.dart';
@@ -17,6 +24,13 @@ import 'package:contact_app1/features/contact/data/datasources/contact_remote_da
 import 'package:contact_app1/features/contact/data/datasources/contact_service.dart';
 import 'package:contact_app1/features/contact/data/repositories/contact_repository_impl.dart';
 import 'package:contact_app1/features/contact/domain/repositories/contact_repository.dart';
+import 'package:contact_app1/features/contact/domain/usecases/create_contact.dart';
+import 'package:contact_app1/features/contact/domain/usecases/delete_contact.dart';
+import 'package:contact_app1/features/contact/domain/usecases/delete_one_contact.dart';
+import 'package:contact_app1/features/contact/domain/usecases/fetch_contacts.dart';
+import 'package:contact_app1/features/contact/domain/usecases/send_email.dart';
+import 'package:contact_app1/features/contact/domain/usecases/toggle_favorite.dart';
+import 'package:contact_app1/features/contact/domain/usecases/update_contact.dart';
 import 'package:contact_app1/features/contact/presentation/bloc/conatct_cubit.dart';
 import 'package:contact_app1/features/user/data/datasources/user_remote_data_source.dart';
 import 'package:contact_app1/features/user/data/datasources/user_remote_data_source_impl.dart';
@@ -24,6 +38,10 @@ import 'package:contact_app1/features/user/data/datasources/user_remote_data_sou
 import 'package:contact_app1/features/user/data/datasources/users_service.dart';
 import 'package:contact_app1/features/user/data/repositories/user_repository_impl.dart';
 import 'package:contact_app1/features/user/domain/repositories/user_repository.dart';
+import 'package:contact_app1/features/user/domain/usecases/create_user.dart';
+import 'package:contact_app1/features/user/domain/usecases/delete_all_users.dart';
+import 'package:contact_app1/features/user/domain/usecases/get_users.dart';
+import 'package:contact_app1/features/user/domain/usecases/update_user.dart';
 import 'package:contact_app1/features/user/presentation/bloc/user_cubit.dart';
 
 import 'package:dio/dio.dart';
@@ -52,6 +70,7 @@ Future<void> init() async {
   sl.registerLazySingleton<ContactService>(() => ContactService(sl()));
   sl.registerLazySingleton<UsersService>(() => UsersService(sl()));
 
+//remot data source
   sl.registerLazySingleton<CompanyRemoteDataSource>(
     () => CompanyRemoteDataSourceImpl(companyService: sl()),
   );
@@ -68,6 +87,7 @@ Future<void> init() async {
     () => AuthRemoteDataSourceImpl(authService: sl()),
   );
 
+//Repository
   sl.registerLazySingleton<AuthRepository>(
     () => AuthRepositoryImpl(authRemoteDataSource: sl()),
   );
@@ -82,13 +102,64 @@ Future<void> init() async {
       authService: sl(),
     ),
   );
+
   sl.registerLazySingleton<UserRepository>(
     () => UserRepositoryImpl(remoteDataSource: sl()),
   );
 
+  ////// Use Cases
+  //company
+  sl.registerLazySingleton<UpdateCompany>(() => UpdateCompany(sl()));
+  sl.registerLazySingleton<GetCompanyDetails>(() => GetCompanyDetails(sl()));
+
+//user
+  sl.registerLazySingleton<CreateUser>(() => CreateUser(sl()));
+  sl.registerLazySingleton<UpdateUser>(() => UpdateUser(sl()));
+  sl.registerLazySingleton<GetUsers>(() => GetUsers(sl()));
+  sl.registerLazySingleton<DeleteAllUsers>(() => DeleteAllUsers(sl()));
+
+  //contsct
+  sl.registerLazySingleton<CreateContact>(() => CreateContact(sl()));
+  sl.registerLazySingleton<UpdateContact>(() => UpdateContact(sl()));
+  sl.registerLazySingleton<FetchContacts>(() => FetchContacts(sl()));
+  sl.registerLazySingleton<DeleteContact>(() => DeleteContact(sl()));
+  sl.registerLazySingleton<DeleteOneContact>(() => DeleteOneContact(sl()));
+  sl.registerLazySingleton<SendEmail>(() => SendEmail(sl()));
+  sl.registerLazySingleton<ToggleFavorite>(() => ToggleFavorite(sl()));
+
+  // auth use cases
+  sl.registerLazySingleton<CheckToken>(() => CheckToken(sl()));
+  sl.registerLazySingleton<GetToken>(() => GetToken(sl()));
+  sl.registerLazySingleton<LoginUser>(() => LoginUser(sl()));
+  sl.registerLazySingleton<LogoutUser>(() => LogoutUser(sl()));
+  sl.registerLazySingleton<RegisterUser>(() => RegisterUser(sl()));
+
   // Cubits
-  sl.registerFactory(() => AuthCubit(authRepository: sl()));
-  sl.registerFactory(() => CompanyCubit(companyRepository: sl()));
-  sl.registerFactory(() => ContactCubit(contactRepository: sl()));
-  sl.registerFactory(() => UserCubit(userRepository: sl()));
+  sl.registerFactory(() => AuthCubit(
+        checkTokenUseCase: sl(),
+        getTokenUseCase: sl(),
+        loginUserUseCase: sl(),
+        registerUserUseCase: sl(),
+        logoutUserUseCase: sl(),
+      ));
+
+  sl.registerFactory(() => CompanyCubit(
+        getCompanyDetailsUseCase: sl(),
+        updateCompanyUseCase: sl(),
+      ));
+
+  sl.registerFactory(() => ContactCubit(
+      createContactUseCase: sl(),
+      deleteContactUseCase: sl(),
+      deleteOneContactUseCase: sl(),
+      fetchContactsUseCase: sl(),
+      sendEmailUseCase: sl(),
+      toggleFavoriteUseCase: sl(),
+      updateContactUseCase: sl()));
+
+  sl.registerFactory(() => UserCubit(
+      createUserUseCase: sl(),
+      deleteAllUsersUseCase: sl(),
+      getUsersUseCase: sl(),
+      updateUserUseCase: sl()));
 }
